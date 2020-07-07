286a03c3dadd71b3fc34fcc98a36669c90ab2348
Alozavander <alohavanhill@qq.com>
2020-07-07 10:41:19 +0800

    The first public lib for my own.

---
+++ myfirstlibrary/src/main/java/com/example/myfirstlibrary/InputTest.java
@@ -0,0 +1,13 @@
+package com.example.myfirstlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class InputTest {
+    private static Toast mToast;
+
+    public static void displayInput(Context pContext, String pStr){
+        mToast = Toast.makeText(pContext,pStr,Toast.LENGTH_LONG);
+        mToast.show();
+    }
+}
